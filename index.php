78a16b97de3d98aa4c6b02f3fd29b78fa7d65de4
John Lloyd Quijano <101556407+lloydskieee@users.noreply.github.com>
2023-02-26 11:55:56 +0800

    INIT

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
             <nav>
                 <ul>
                     <li>
-                        <a href="/movies/movies.php">Movies</a>
+                        <a href="/movies/movies.html">Movies</a>
                     </li>
                     <li class="#">
                         <a href="/tv-shows/tv-shows.html">TV Shows</a>
@@ -28,7 +28,7 @@
         <main>
             <section>
                 <h2 class="container">Welcome to Mubiee 2.0</h2>
-                <div class="mainBtn"><a href="/movies/movies.php" class="download-btn">Go to homepage <i class="fa-solid fa-circle-right"></i></a></div>
+                <div class="mainBtn"><a href="/movies/movies.html" class="download-btn">Go to homepage <i class="fa-solid fa-circle-right"></i></a></div>
                 <p> Mubiee is a free online stream to watch movies and tv shows. It has an extensive collection of content that includes all the latest releases, popular classics, award-winning films, and family favorites. Mubiee offers users the opportunity to watch free movies online without having to download anything or create any accounts. All you have to do is select what you want to watch, press play, and enjoy! </p>
                 <p> Watching movies online has never been easier. Mubiee provides users with the option to watch their favorite tv shows and episodes in full-screen mode, which is great for when you're trying to watch a movie at work but don't want anyone around your cubicle catching an eyeful of what's on your screen. </p>
                 <p> One of the great things about Mubiee is that you can watch tv shows and movies from all different genres, including action, adventure, comedy, family-friendly films, superhero flicks (we love those), horror pictures for those who enjoy a good scare at night before bedtime, documentaries with interesting facts about people, places, and things around the world (check out our article on watch documentaries online free here), or musicals for those who simply enjoy watching talented dancers bust a move. </p>
